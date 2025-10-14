251623c2c2ad98251420ff65d6cd97993890c2c7
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee182@gmail.com>
2025-10-15 05:15:46 +0530

    WIP: client-api...

--- public/js/player/client-player-api.js
+++ public/js/player/client-player-api.js
@@ -42,6 +42,27 @@ async function getHeaders() {
   };
 }
 
-async function transferPlayback(device_id, shouldPlay = false) {}
+/**
+ * Transfer playback to a new device and optionally begin playback.
+ * Only works with Spotify Premium: https://developer.spotify.com/documentation/web-api/reference/transfer-a-users-playback
+ *
+ * @param {string} deviceId -  The ID of the device on which playback should be started/transferred
+ * @param {boolean} shouldPlay - Decides whether playback happens on the intended device
+ * @returns {Promise<void>}
+ */
+async function transferPlayback(deviceId, shouldPlay = false) {
+  try {
+    const headers = await getHeaders();
+    const reqBody = { device_ids: [deviceId], play: shouldPlay };
+
+    await fetch(`${BASE_URL}/me/player`, {
+      headers,
+      method: "PUT",
+      body: JSON.stringify(reqBody),
+    });
+  } catch (error) {
+    console.error("Error transferring playback to the intended device:", error);
+  }
+}
 
 export { BASE_URL, getAccessToken, getHeaders, transferPlayback };
--- public/js/player/client-player.js
+++ public/js/player/client-player.js
@@ -5,7 +5,46 @@
 
 "use strict";
 
+// Docs: https://developer.spotify.com/documentation/web-playback-sdk/tutorials/getting-started
+
 import { getAccessToken, transferPlayback } from "./client-player-api.js";
+
+const $players = document.querySelectorAll("[data-player]");
+
+function updatePlayerUI($player, playerState) {
+  const $trackBanner = $player.querySelector("[data-track-banner]");
+  const $trackName = $player.querySelector("[data-track-name]");
+  const $trackArtists = $player.querySelector("[data-track-artists]");
+  const {
+    track_window: {
+      current_track: {
+        album: { images },
+        artists,
+        name: trackName,
+      },
+    },
+  } = playerState;
+  const {
+    width,
+    height,
+    url = "/images/track-banner.png",
+  } = images.find((img) => img.width > 300 && img.width < 400);
+  const artistNames = artists.map(({ name }) => name).join(", ");
+  $trackBanner.src = url;
+  $trackBanner.width = width;
+  $trackBanner.height = height;
+  $trackBanner.alt = `${$trackName} cover image`;
+  $trackName.innerText = $trackName;
+  $trackArtists.innerText = artistNames;
+  $player.classList.remvoe("hide");
+  $player.classList.remvoe("disabled");
+}
+
+function handlePlayerStateChange(playerState) {
+  const { track_window } = playerState;
+  console.log(track_window);
+  $players.forEach(($player) => updatePlayerUI($player, playerState));
+}
 
 window.onSpotifyWebPlaybackSDKReady = async () => {
   const token = await getAccessToken();
@@ -25,8 +64,10 @@ window.onSpotifyWebPlaybackSDKReady = async () => {
     // console.log("Ready with Device ID", device_id);
 
     localStorage.setItem("device_id", device_id);
-    transferPlayback(device_id);
+    transferPlayback(device_id); // transfers playback to the current device
   });
+
+  player.addListener("player_state_changed", handlePlayerStateChange);
 
   player.addListener("not_ready", ({ device_id }) => {
     console.log("Device ID has gone offline", device_id);
